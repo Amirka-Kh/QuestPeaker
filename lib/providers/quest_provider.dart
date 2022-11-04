4b5c22c40ae87bfbb6077fc6e34bde6e6a8d50ae
Amir <66668728+Amirka-Kh@users.noreply.github.com>
2022-11-04 19:45:15 +0300

    Add description for quests

--- lib/providers/quest_provider.dart
+++ lib/providers/quest_provider.dart
@@ -5,13 +5,13 @@ List questList = <Quest>[
   Quest(
     'Robotics Lab',
     'assets/quest/robot.png',
-    'bla-bla',
+    'the newest invention "native robot" was lost in the laboratories of labor technology. The new invention has intellect and senses, it is not large and can move independently. Engineers report that shortly before lunch, a quarrel broke out between the two scientists, which could have prompted the robot to escape. Ask scientists and find a robot',
     [QuestColor.lightGreen, QuestColor.green],
   ),
   Quest(
     'Machine Rise',
     'assets/quest/calculator.png',
-    'bla-bla',
+    'At Innopolis University, we are registering more and more cases of equipment failure. The professors are worried that this might be the work of an evil ai. Investigate this case and find out if the machines really started the uprising.',
     [QuestColor.darkRed, QuestColor.red],
   ),
 ];
