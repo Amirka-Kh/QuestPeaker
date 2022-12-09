1a463be4a779833a01817df653cca0e4f948f978
Amir <amirabikhuja@gmail.com>
2022-12-10 02:04:05 +0300

    Update quest.dart

--- lib/pages/home/widgets/quest.dart
+++ lib/pages/home/widgets/quest.dart
@@ -6,12 +6,19 @@ import 'package:quest_peak/domain/models/quest_model.dart';
 import 'package:quest_peak/pages/home/widgets/quest_details.dart';
 import 'package:quest_peak/pages/home/widgets/to_favorite_button.dart';
 import 'package:quest_peak/pages/home/widgets/is_solved.dart';
-import 'package:quest_peak/config/styles.dart';
+// import 'package:quest_peak/config/styles.dart';
 
+// ignore: must_be_immutable
 class QuestWidget extends ConsumerWidget {
-  final Quest quest;
+  late Quest quest;
+  late PageController pageController;
+  late int currentPage;
 
-  const QuestWidget({super.key, required this.quest});
+  QuestWidget(
+      {super.key,
+      required this.quest,
+      required this.pageController,
+      required this.currentPage});
 
   // user will interact with a widget, thus Gesture detection is needed
   // for this purposes InkWell is used since it  shows a visual indication
@@ -23,74 +30,93 @@ class QuestWidget extends ConsumerWidget {
     final appTheme = ref.watch(themeProvider);
 
     return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          PageRouteBuilder(
-            transitionDuration: const Duration(milliseconds: 400),
-            pageBuilder: (context, _, __) {
-              return QuestDetailsWidget(quest: quest);
-            },
-          ),
-        );
-      },
-      child: Stack(
-        children: [
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ClipPath(
-              clipper: QuestCardClipper(),
-              child: Container(
-                height: 0.7 * screenHeight,
-                width: 0.9 * screenWidth,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: toListColor(quest.colors),
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
+        onTap: () {
+          Navigator.push(
+            context,
+            PageRouteBuilder(
+              transitionDuration: const Duration(milliseconds: 400),
+              pageBuilder: (context, _, __) {
+                return QuestDetailsWidget(quest: quest);
+              },
+            ),
+          );
+        },
+        child: AnimatedBuilder(
+            animation: pageController,
+            builder: (context, child) {
+              double value = 1;
+              if (pageController.position.haveDimensions) {
+                value = pageController.page! - currentPage;
+                value = (1 - (value.abs() * 0.6)).clamp(0.0, 1.0);
+              }
+
+              return Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30.0),
+                      child: Hero(
+                        tag: "background-${quest.name}",
+                        child: Container(
+                          height: 0.6 * screenHeight,
+                          width: 0.9 * screenWidth,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: toListColor(quest.colors),
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-          ),
-          Align(
-            alignment: const Alignment(0, 0.5),
-            child: Image.asset(
-              quest.imagePath,
-              height: screenHeight * 0.5,
-              width: screenWidth * 0.7,
-            ),
-          ),
-          Align(
-            alignment: const Alignment(0.8, 0.7),
-            child: IsSolvedWidget(quest: quest),
-          ),
-          Align(
-            alignment: const Alignment(0.8, 0.95),
-            child: SaveToFavouritesWidget(quest: quest),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 48, right: 16, bottom: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Material(
-                  color: Colors.transparent,
-                  child: Text(
-                    quest.name,
-                    style: appTheme.heading(),
+                  Align(
+                    alignment: const Alignment(0, 0.2),
+                    child: Hero(
+                      tag: "image-${quest.name}",
+                      child: ClipOval(
+                        child: Image.asset(
+                          quest.imagePath,
+                          height: screenHeight * 0.35 * value,
+                          width: screenWidth * 0.7,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                Text(
-                  AppLocalizations.of(context)!.learnMore,
-                  style: appTheme.subHeading(),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+                  Align(
+                    alignment: const Alignment(0.8, 0.7),
+                    child: IsSolvedWidget(quest: quest),
+                  ),
+                  Align(
+                    alignment: const Alignment(0.8, 0.95),
+                    child: SaveToFavouritesWidget(quest: quest),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 48, right: 16, bottom: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Material(
+                          color: Colors.transparent,
+                          child: Text(
+                            quest.name,
+                            style: appTheme.heading(),
+                          ),
+                        ),
+                        Text(
+                          AppLocalizations.of(context)!.learnMore,
+                          style: appTheme.subHeading(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            }));
   }
 }
